210e4e1fba32d70cb6e0d75c90b309754f421b67
com140 <riddhima.kejriwal@daffodilsw.com>
2024-09-20 13:43:40 +0530

    changes done

--- budget_script.js
+++ budget_script.js
@@ -3,3 +3,14 @@ var userName = prompt("Enter your name");
 if(userName != null) {
     document.getElementById("name").innerHTML = userName;
 }
+
+function getLocalStorage(a) {
+    return JSON.parse(localStorage.getItem(a));
+}
+
+function setLocalStorage(a, b) {
+    localStorage.setItem(a , JSON.stringify(b));
+}
+
+
+
